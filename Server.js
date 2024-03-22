06517baa63393d8edb17decb7f1537dd7e65bc50
ahammed shefin <ahammedshefin1@gmail.com>
2024-03-22 07:04:09 -0700

    changed the cors link to https://mern-series.netlify.app/

--- Server.js
+++ Server.js
@@ -5,10 +5,10 @@ const app = express();
 const userrouter = require("./Router/User");
 const adminrouter = require("./Router/Admin");
 const connectdb = require("./Utility/db");
-const authmiddleware=require('./middleware/Auth')
+const authmiddleware = require("./middleware/Auth");
 
 const corsOption = {
-    origin: "http://localhost:5173",
+    origin: "https://mern-series.netlify.app",
     methods: "GET, POST, PUT, DELETE, PATCH ,HEAD",
     Credentials: true,
 };
@@ -18,7 +18,7 @@ app.use(cors(corsOption));
 app.use(express.json());
 
 app.use("/", userrouter);
-app.use("/admin",adminrouter );
+app.use("/admin", adminrouter);
 const PORT = process.env.PORT;
 
 connectdb().then(() => {
